f8bd15a22180e6a980187e493d0abfc5ae8c6db0
Simon Villaverde <simonvillaverde9@gmail.com>
2021-10-22 15:28:33 -0300

    adding computer ships logic

--- src/components/ComputerBoard.js
+++ src/components/ComputerBoard.js
@@ -2,57 +2,77 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useEffect, useState } from 'react';
 import { PropTypes } from 'prop-types';
-import { useSelector } from 'react-redux';
+
+const computerShipsAvaibles = [
+  {
+    name: 'carrier',
+    amount: 1,
+    spaces: 4,
+  },
+  {
+    name: 'cruisers',
+    amount: 3,
+    spaces: 3,
+  },
+  {
+    name: 'submarine',
+    amount: 1,
+    spaces: 2,
+  },
+];
 
 const ComputerBoard = ({ COLUMNS, ROWS }) => {
   // eslint-disable-next-line no-unused-vars
   const [layout, setLayout] = useState(new Array(ROWS * COLUMNS).fill('empty'));
-  const states = useSelector((state) => state);
 
   useEffect(() => {
     const generateComputerLayout = () => {
-      const totalShips = states.shipsAvaibles.filter((ship) => ship.amount > 0);
-      const totalAmount = states.shipsAvaibles.reduce(
-        (prevValue, currValue) => prevValue + currValue.amount,
-        0,
-      );
+      const totalShips = computerShipsAvaibles;
+      // const totalAmount = computerShipsAvaibles.reduce(
+      //   (prevValue, currValue) => prevValue + currValue.amount,
+      //   0,
+      // );
 
       const newLayout = [...layout];
-      console.log(totalShips, totalAmount, newLayout);
-
-      // for (let j = 0; j < totalAmount; j += 1) {
-      //   console.log(j);
-      // }
 
       // Iterate over all types of ships
       for (let j = 0; j < totalShips.length; j += 1) {
-        console.log(totalShips[j]);
         // Iterate over the amount of the specific ship
         for (let k = 0; k < totalShips[j].amount; k += 1) {
-          console.log(totalShips[j].amount);
+          const i = Math.floor(Math.random() * (COLUMNS * ROWS));
+          const x = i % ROWS;
+          const y = Math.floor(i / COLUMNS);
+          const isHorizontal = Math.floor(Math.random() * 2) === 1;
+
+          // Iterate over the spaces of the specific ship
+          for (let n = 0; n < totalShips[j].spaces; n += 1) {
+            // Check if the ship fits in the board
+
+            if (isHorizontal && x + totalShips[k].spaces <= COLUMNS) {
+              console.log('horizontal');
+              newLayout[i + n] = 'ship';
+            } else if (!isHorizontal && y + totalShips[k].spaces <= ROWS) {
+              console.log('vertical');
+              newLayout[i + COLUMNS * n] = 'ship';
+            }
+
+            // if (isHorizontal && x + totalShips[k].spaces <= COLUMNS) {
+            //   if (newLayout[i + k] === 'ship') return;
+            //   newLayout[i + k] = 'ship';
+            //   console.log('a');
+            // } else if (!isHorizontal && y + totalShips[k].spaces <= ROWS) {
+            //   if (newLayout[i + COLUMNS * k] === 'ship') return;
+            //   newLayout[i + COLUMNS * k] = 'ship';
+            //   console.log('aa');
+            // } else {
+            //   console.log('aaa');
+            //   return;
+            // }
+          }
         }
       }
 
-      // const i = Math.floor(Math.random() * (COLUMNS * ROWS));
-      // const isHorizontal = Math.floor(Math.random() * 2) === 1;
-
-      // const x = i % ROWS;
-      // const y = Math.floor(i / COLUMNS);
-
-      // for (let j = 0; j < states.ship?.spaces; j += 1) {
-      //   if (states.ship.amount < 1) return;
-
-      //   // Check if the ship fits in the board
-      //   if (isHorizontal && x + states.ship.spaces <= COLUMNS) {
-      //     if (newLayout[i + j] === 'ship') return;
-      //     newLayout[i + j] = 'ship';
-      //   } else if (!isHorizontal && y + states.ship.spaces <= ROWS) {
-      //     if (newLayout[i + COLUMNS * j] === 'ship') return;
-      //     newLayout[i + COLUMNS * j] = 'ship';
-      //   } else {
-      //     return;
-      //   }
-      // }
+      setLayout(newLayout);
     };
 
     generateComputerLayout();
@@ -68,7 +88,7 @@ const ComputerBoard = ({ COLUMNS, ROWS }) => {
           <div
             // eslint-disable-next-line react/no-array-index-key
             key={index}
-            className={`square ${square}`}
+            className={`square ${square} computer`}
           />
         ))}
       </div>
